9fc10459111a8f9744f66260063b3e56ac99ff80
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-01-21 12:12:41 -0800

    working overlab between genes and QTL support intervals

--- scripts/brassica_gff_database.R
+++ scripts/brassica_gff_database.R
@@ -6,19 +6,20 @@ setwd("/Users/Cody_2/git.repos/brassica_genome_db/raw_data")
 ?system.file
 ?makeTranscriptDbFromGFF
 ?read.table
-test <- read.table("brassica_rapa_final.gtf", header = FALSE)
-head(test)
-str(test)
+# test <- read.table("brassica_rapa_final.gtf", header = FALSE)
+# head(test)
+# str(test)
 
-test2 <- read.table("brassica_rapa_final.bed", header = FALSE)
-head(test2)
-str(test2)
+# update with new genome mapping
+br_bed <- read.table("Brassica_rapa_v1.5_final.bed", header = FALSE)
+head(br_bed)
+str(br_bed)
 
 #format for entry in database
 #subset columns required by makeTranscriptDb
 
 #transcripts data frame
-transcripts <- test2[,c("V4","V4","V1","V6","V2","V3")]
+transcripts <- br_bed[,c("V4","V4","V1","V6","V2","V3")]
 head(transcripts)
 tail(transcripts)
 
@@ -42,42 +43,40 @@ splicings <- transcripts
 head(splicings)
 
 # splicings number is 1 for each gene
-splicings$exon_rank <- rep(1,nrow(splicings))
+splicings$exon_rank <- rep(1, nrow(splicings))
 head(splicings)
 
 # rearrange splicings with only mandatory columns
 # rename columns
-splicings <- splicings[,c(1,7,5,6)]
+splicings <- splicings[, c(1,7,5,6)]
 head(splicings)
 colnames(splicings) <- c("tx_id", "exon_rank", "exon_start", "exon_end")
-
+head(splicings)
 
 
 brassica_db <- makeTranscriptDb(transcripts, splicings)
 brassica_db
 # succes
-head(brassica_db)
-
-?extractTranscriptSeqs
-gene <- extractTranscriptSeqs()
-
-
 class(brassica_db)
 ?RangedData
-
 ?GRanges
-gr <- GRanges(seqnames = "A10",
-              ranges = IRanges(start=16032467, end=16230621))
 
-gr
-transcriptsByOverlaps(brassica_db,gr)
-
-
+#example QTL output
 # > bayesint(out_em_2, "A10", prob = 0.98, expandtomarkers = TRUE)
 #              chr      pos      lod
 # A10_16230621 A10 82.90340 13.10773
 # A10_16069633 A10 83.30983 14.08732
 # A10_16032467 A10 84.54993 12.45285
 
+#simple test for QTL range
+gr <- GRanges(seqnames = "A10",
+              ranges = IRanges(start=16032467, end=16230621))
+
+gr
+
+#look for overlaps between QTL support interval and transcripts
+transcripts_qtl <- transcriptsByOverlaps(brassica_db, gr)
+# GRanges object with 48 ranges and 2 metadata columns:
+transcripts_qtl
 
 
