619f0c15cf74a91a908395a6de7d4a32390ccc76
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-04-01 22:22:03 -0700

    visualize whole graph

--- scripts/n4j_tutorial.R
+++ scripts/n4j_tutorial.R
@@ -36,10 +36,10 @@ browse(graph)
 #species: name:
 #genotype name: 
 ?addConstraint
-addConstraint(graph, "genotype", "name")
-RIL_2 <- createNode(graph, c("genotype","Brassica"), name = "RIL_2", species = "Brassica rapa")
-RIL_1 <- createNode(graph, c("genotype","Brassica"), name = "RIL_1", species = "Brassica rapa")
-RIL_3 <- createNode(graph, c("genotype","Brassica"), name = "RIL_3", species = "Brassica rapa")
+addConstraint(graph, "genotype", "genotype")
+RIL_2 <- createNode(graph, c("genotype","Brassica"), genotype = "RIL_2")
+RIL_1 <- createNode(graph, c("genotype","Brassica"), genotype = "RIL_1")
+RIL_3 <- createNode(graph, c("genotype","Brassica"), genotype = "RIL_3")
 
 #marker name:
 addConstraint(graph, "marker", "name")
@@ -52,46 +52,165 @@ mk_3 <- createNode(graph, c("marker","Brassica"), name = "A03x9953796", chr = "A
 ##investigation
 ##treatment
 #individual
-RIL_1_cr <- createNode(graph, c("investigation","treatment"), name = "RIL1_cr",
-              genotype = "RIL_1", trt = "cr", exp = "edwards")
-RIL_2_cr <- createNode(graph, c("investigation","treatment"), name = "RIL2_cr",
-               genotype = "RIL_2", trt = "cr", exp = "edwards")
-RIL_3_cr <- createNode(graph, c("investigation","treatment"), name = "RIL3_cr",
-                      genotype = "RIL_3", trt = "cr", exp = "edwards")
+addConstraint(graph, "Observation", "observation")
+obs_1 <- createNode(graph, c("Observation"), observation = "1", value = 6, units = "cm")
+obs_2 <- createNode(graph, c("Observation"), observation = "2 ", value = 5, units = "cm")
+obs_3 <- createNode(graph, c("Observation"), observation = "3", value = 10, units = "cm")
 
-RIL_1_un <- createNode(graph, c("investigation","treatment"), name = "RIL1_un",
-                 genotype = "RIL_1", trt = "un", exp = "edwards")
-RIL_2_un <- createNode(graph, c("investigation","treatment"), name = "RIL2_un",
-                 genotype = "RIL_2", trt = "un", exp = "edwards")
-RIL_3_un <- createNode(graph, c("investigation","treatment"), name = "RIL3_un",
-                 genotype = "RIL_3", trt = "un", exp = "edwards")
+obs_4 <- createNode(graph, c("Observation"), observation = "4", value = 20, units = "cm")
+obs_5 <- createNode(graph, c("Observation"), observation = "5", value = 25, units = "cm")
+obs_6 <- createNode(graph, c("Observation"), observation = "6", value = 25, units = "cm")
 
-#trait
-met_1_1 <- createNode(graph, c("trait","metabolite","leaf","shoot"),
-                   name = "3-Hydroxypropyl")
-met_1_2 <- createNode(graph, c("trait","metabolite","root"),
-                   name = "3-Hydroxypropyl")
-met_1_3 <- createNode(graph, c("metabolite"), name = "3-Hydroxypropyl",
-                       URL = "http://tinyurl.com/q8zrhsq")
+obs_7 <- createNode(graph, c("Observation"), observation = "7", value = 50, units = "ret time")
+obs_8 <- createNode(graph, c("Observation"), observation = "8", value = 100, units = "ret time")
+obs_9 <- createNode(graph, c("Observation"), observation = "9", value = 34, units = "ret time")
+
+obs_10 <- createNode(graph, c("Observation"), observation = "10", value = 50, units = "ret time")
+obs_11 <- createNode(graph, c("Observation"), observation = "11", value = 100, units = "ret time")
+obs_12 <- createNode(graph, c("Observation"), observation = "12", value = 34, units = "ret time")
+
+#time varying observations
+obs_13 <- createNode(graph, c("Observation"), observation = "13", value = 50, units = "cm")
+obs_14 <- createNode(graph, c("Observation"), observation = "14", value = 100, units = "cm")
+obs_15 <- createNode(graph, c("Observation"), observation = "15", value = 1500, units = "cm")
+obs_13
+#investigations
+addConstraint(graph, "Investigation", "Investigation")
+exp_1 <- createNode(graph, c("Investigation"), Investigation = "edwards")
+exp_2 <- createNode(graph, c("Investigation"), Investigation = "brock")
+exp_3 <- createNode(graph, c("Investigation"), Investigation = "duchaine")
+
+#treatments
+addConstraint(graph, "Treatment", "Treatment")
+trt_1 <- createNode(graph, c("Treatment"), Treatment = "Uncrowded")
+trt_2 <- createNode(graph, c("Treatment"), Treatment = "Crowded")
+trt_3 <- createNode(graph, c("Treatment"), Treatment = "Drought")
+
+#structures
+addConstraint(graph, "Structure", "Structure")
+leaf <- createNode(graph, c("Structure"), Structure = "leaf")
+petiole <- createNode(graph, c("Structure"), Structure = "petiole")
+root <- createNode(graph, c("Structure"), Structure = "root")
+
+
+#traits
+# met_1_1 <- createNode(graph, c("metabolite","trait","leaf","shoot"),
+                   # name = "3-Hydroxypropyl")
+leaflength <- createNode(graph, c("leaflength","trait"), trait = "leaflength")
+petiolelength <- createNode(graph, c("petiolelength","trait"),
+                   trait = "petiolelength")
+metab <- createNode(graph, c("metabolite","trait"),
+                     name = "3-Hydroxypropyl", URL = "http://tinyurl.com/q8zrhsq")
 #delete(met_1_1)
 
-#create some genotype trait relationships
-rel_1 <- createRel(RIL_1_cr, "HAS_TRAIT", met_1_1, value = 1.2)
-rel_2 <- createRel(RIL_2_cr, "HAS_TRAIT", met_1_1, value = 2)
-rel_3 <- createRel(RIL_3_cr, "HAS_TRAIT", met_1_1, value = 10)
-rel_4 <- createRel(RIL_1_un, "HAS_TRAIT", met_1_1, value = 1)
-rel_5 <- createRel(RIL_2_un, "HAS_TRAIT", met_1_1, value = 5)
-rel_6 <- createRel(RIL_3_un, "HAS_TRAIT", met_1_1, value = 6)
 
-rel_2_1 <- createRel(RIL_1_cr, "HAS_TRAIT", met_1_2, value = 1.5)
-rel_2_2 <- createRel(RIL_2_cr, "HAS_TRAIT", met_1_2, value = 6)
-rel_2_3 <- createRel(RIL_3_cr, "HAS_TRAIT", met_1_2, value = 5)
-rel_2_4 <- createRel(RIL_1_un, "HAS_TRAIT", met_1_2, value = 4)
-rel_2_5 <- createRel(RIL_2_un, "HAS_TRAIT", met_1_2, value = 1)
-rel_2_6 <- createRel(RIL_3_un, "HAS_TRAIT", met_1_2, value = 8)
+######
+#RELATIONSHIPS
+######
+#obs 1 to 6
+createRel(obs_1, "IN_EXPERIMENT", exp_1)
+createRel(obs_2, "IN_EXPERIMENT", exp_1)
+createRel(obs_3, "IN_EXPERIMENT", exp_1)
+createRel(obs_4, "IN_EXPERIMENT", exp_1)
+createRel(obs_5, "IN_EXPERIMENT", exp_1)
+createRel(obs_6, "IN_EXPERIMENT", exp_1)
 
-rel_met_1 <- createRel(met_1_1, "IS_METABOLITE", met_1_3)
-rel_met_1 <- createRel(met_1_2, "IS_METABOLITE", met_1_3)
+createRel(obs_1, "IN_TREATMENT", trt_1)
+createRel(obs_2, "IN_TREATMENT", trt_1)
+createRel(obs_3, "IN_TREATMENT", trt_1)
+createRel(obs_4, "IN_TREATMENT", trt_2)
+createRel(obs_5, "IN_TREATMENT", trt_2)
+createRel(obs_6, "IN_TREATMENT", trt_2)
+
+createRel(obs_1, "IN_STRUCTURE", petiole)
+createRel(obs_2, "IN_STRUCTURE", petiole)
+createRel(obs_3, "IN_STRUCTURE", petiole)
+createRel(obs_4, "IN_STRUCTURE", petiole)
+createRel(obs_5, "IN_STRUCTURE", petiole)
+createRel(obs_6, "IN_STRUCTURE", petiole)
+
+createRel(obs_1, "TRAIT", leaflength)
+createRel(obs_2, "TRAIT", leaflength)
+createRel(obs_3, "TRAIT", leaflength)
+createRel(obs_4, "TRAIT", leaflength)
+createRel(obs_5, "TRAIT", leaflength)
+createRel(obs_6, "TRAIT", leaflength)
+
+createRel(obs_1, "IS_GENOTYPE", RIL_1)
+createRel(obs_2, "IS_GENOTYPE", RIL_2)
+createRel(obs_3, "IS_GENOTYPE", RIL_3)
+createRel(obs_4, "IS_GENOTYPE", RIL_1)
+createRel(obs_5, "IS_GENOTYPE", RIL_2)
+createRel(obs_6, "IS_GENOTYPE", RIL_3)
+
+######
+#RELATIONSHIPS
+######
+#obs 7 to 12
+createRel(obs_7, "IN_EXPERIMENT", exp_2)
+createRel(obs_8, "IN_EXPERIMENT", exp_2)
+createRel(obs_9, "IN_EXPERIMENT", exp_2)
+createRel(obs_10, "IN_EXPERIMENT", exp_2)
+createRel(obs_11, "IN_EXPERIMENT", exp_2)
+createRel(obs_12, "IN_EXPERIMENT", exp_2)
+
+createRel(obs_7, "IN_TREATMENT", trt_1)
+createRel(obs_8, "IN_TREATMENT", trt_1)
+createRel(obs_9, "IN_TREATMENT", trt_1)
+createRel(obs_10, "IN_TREATMENT", trt_2)
+createRel(obs_11, "IN_TREATMENT", trt_2)
+createRel(obs_12, "IN_TREATMENT", trt_2)
+
+createRel(obs_7, "IN_STRUCTURE", leaf)
+createRel(obs_8, "IN_STRUCTURE", leaf)
+createRel(obs_9, "IN_STRUCTURE", leaf)
+createRel(obs_10, "IN_STRUCTURE", leaf)
+createRel(obs_11, "IN_STRUCTURE", leaf)
+createRel(obs_12, "IN_STRUCTURE", leaf)
+
+createRel(obs_7, "TRAIT", metab)
+createRel(obs_8, "TRAIT", metab)
+createRel(obs_9, "TRAIT", metab)
+createRel(obs_10, "TRAIT", metab)
+createRel(obs_11, "TRAIT", metab)
+createRel(obs_12, "TRAIT", metab)
+
+createRel(obs_7, "IS_GENOTYPE", RIL_1)
+createRel(obs_8, "IS_GENOTYPE", RIL_2)
+createRel(obs_9, "IS_GENOTYPE", RIL_3)
+createRel(obs_10, "IS_GENOTYPE", RIL_1)
+createRel(obs_11, "IS_GENOTYPE", RIL_2)
+createRel(obs_12, "IS_GENOTYPE", RIL_3)
+
+
+#####
+#RELATIONSHIPS
+######
+#obs 13 15
+createRel(obs_13, "IN_EXPERIMENT", exp_3)
+createRel(obs_14, "IN_EXPERIMENT", exp_3)
+createRel(obs_15, "IN_EXPERIMENT", exp_3)
+
+createRel(obs_13, "IN_TREATMENT", trt_3)
+createRel(obs_14, "IN_TREATMENT", trt_3)
+createRel(obs_15, "IN_TREATMENT", trt_3)
+
+createRel(obs_13, "IN_STRUCTURE", leaf)
+createRel(obs_14, "IN_STRUCTURE", leaf)
+createRel(obs_15, "IN_STRUCTURE", leaf)
+
+createRel(obs_13, "TRAIT", metab)
+createRel(obs_14, "TRAIT", metab)
+createRel(obs_15, "TRAIT", metab)
+
+createRel(obs_13, "TIME", obs_14)
+createRel(obs_14, "TIME", obs_15)
+
+createRel(obs_13, "IS_GENOTYPE", RIL_1)
+createRel(obs_14, "IS_GENOTYPE", RIL_2)
+createRel(obs_15, "IS_GENOTYPE", RIL_3)
+
+
 
 #genotype combo
 rel_geno1 <- createRel(RIL_1, "HAS_GENOTYPE", mk_1, genotype = "AA")
@@ -106,14 +225,18 @@ rel_geno1_2 <- createRel(RIL_3, "HAS_GENOTYPE", mk_1, genotype = "BB")
 rel_geno2_2 <- createRel(RIL_3, "HAS_GENOTYPE", mk_2, genotype = "BB")
 rel_geno3_2 <- createRel(RIL_3, "HAS_GENOTYPE", mk_3, genotype = "BB")
 
-createRel(RIL_1_cr, "IS_GENOTYPE", RIL_1)
-createRel(RIL_1_un, "IS_GENOTYPE", RIL_1)
-createRel(RIL_2_cr, "IS_GENOTYPE", RIL_2)
-createRel(RIL_2_cr, "IS_GENOTYPE", RIL_2)
-createRel(RIL_3_cr, "IS_GENOTYPE", RIL_3)
-createRel(RIL_3_cr, "IS_GENOTYPE", RIL_3)
 
+QTL_met <- createNode(graph, c("QTL"), name = "QTL_met")
+QTL_leaf <- createNode(graph, c("QTL"), name = "QTL_leaf")
 
+createRel(metab, "HAS_QTL", QTL_met)
+createRel(QTL_met, "QTL_LOCATION", mk_1, lod = 4.6, R500_allele = 5, IMB_allele = 0.1)
+createRel(QTL_met, "QTL_LOCATION", mk_3, lod = 10, R500_allele = 4, IMB_allele = 2)
+
+createRel(leaflength, "HAS_QTL", QTL_leaf)
+createRel(QTL_leaf, "QTL_LOCATION", mk_1, lod = 20, R500_allele = 5, IMB_allele = 0.1)
+
+browse(graph)
 
 #time
 #leaf
